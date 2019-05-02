2e688a4b4fa96f70840cd862600b87d6e7306960
Shante Austin <shante.austin19@gmail.com>
2019-05-02 18:40:04 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
 class App extends Component {
-  render() {
+    state = {
+      keyCode: "Press key"
+  }
+
+  componentDidMount (){
+    document.addEventListener("keydown", this.setKeyCode);
+  }
+  setKeyCode = (e) => {
+    this.setState({
+      keyCode: e.keyCode,
+      key: e.key,
+      which: e.which,
+      code: e.code
+    });
+  }
+
+  render =() => {
+    const { keyCode, key, which, code } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="App__KeyCode">{keyCode}</div>
+        {keyCode !== "Press key" && (
+          <div className="Box__Container">
+            <div className="Box">
+              <h2>event.key</h2>
+              <span>{key}</span>
+            </div>
+            <div className="Box">
+              <h2>event.which</h2>
+              <span>{which}</span>
+            </div>
+            <div className="Box">
+              <h2>event.code</h2>
+              <span>{code}</span>
+            </div>
+          </div>
+        )}
       </div>
     );
   }
 }
+
 
 export default App;
